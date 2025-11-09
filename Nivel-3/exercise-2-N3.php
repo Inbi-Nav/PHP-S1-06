c9f9358bf337e0c8840a9b2e64a1ebd455203f8e
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-09 15:12:27 +0100

    Nivel 3 exercise 2: create first composer program

--- Nivel-3/exercise-2-N3.php
+++ Nivel-3/exercise-2-N3.php
@@ -11,9 +11,8 @@ echo "<strong>Current time: </strong>" . $today->format('H:i:s') . "<br>" ;
 $tomorrow = $today->addDay();
 echo "<strong>Tomorrow is: </strong>" . $tomorrow->format('d/m/Y') . "<br>" ;
 
-
 $birthday = Carbon::create(2014, 7, 11);
 $age = $birthday->age;
 
-echo "If Maggie was born in " . "<strong>" . $birthday->format('d/m/Y') . "</strong>" . " She is " .  "<strong>" .$age . " years old !" ."</strong>" ;
+echo "If Maggie was born in " . "<strong>" . $birthday->format('d/m/Y') . "</strong>" . " She is " .  "<strong>" .$age . " years old!" ."</strong>" ;
 ?>
